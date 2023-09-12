c84c81611104bd775dbef0a27bb191452aa2f873
Mughees Warraich <m.mughees@Mugheess-Air.lc.luther.edu>
2023-09-12 11:57:28 -0500

    Finish project 0

--- exercises/primes/primes.js
+++ exercises/primes/primes.js
@@ -10,7 +10,10 @@
  * @param {string} name visitor's name
  * @param {string} selector element to use for display
  */
-function greet(name, selector) {}
+function greet(name, selector) {
+    const greetingElement = document.querySelector(selector);
+    greetingElement.textContent = `Welcome, ${name}!`;
+}
 
 
 /**
@@ -19,7 +22,19 @@ function greet(name, selector) {}
  * @param {number} number number to check
  * @return {boolean} result of the check
  */
-function isPrime(number) {}
+function isPrime(number) {
+    if (number <= 1) return false;
+    if (number <= 3) return true;
+    if (number % 2 === 0 || number % 3 === 0) return false;
+
+    for (let i = 5; i * i <= number; i += 6) {
+        if (number % i === 0 || number % (i + 2) === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 
 /**
@@ -28,7 +43,12 @@ function isPrime(number) {}
  * @param {number} number number to check
  * @param {string} selector element to use for display
  */
-function printNumberInfo(number, selector) {}
+function printNumberInfo(number, selector) {
+    const numberInfoElement = document.querySelector(selector);
+    const isPrimeMessage = isPrime(number) ? 'is prime' : 'is not prime';
+    numberInfoElement.textContent = `${number} ${isPrimeMessage}`;
+}
+
 
 
 /**
@@ -37,7 +57,19 @@ function printNumberInfo(number, selector) {}
  * @param {number} number number of primes to generate
  * @return {number[]} an array of `number` prime numbers
  */
-function getNPrimes(number) {}
+function getNPrimes(number) {
+    const primes = [];
+    let currentNumber = 2;
+
+    while (primes.length < number) {
+        if (isPrime(currentNumber)) {
+            primes.push(currentNumber);
+        }
+        currentNumber++;
+    }
+
+    return primes;
+}
 
 
 /**
@@ -46,7 +78,20 @@ function getNPrimes(number) {}
  * @param {number} number number of primes to display
  * @param {string} selector element to use for display
  */
-function printNPrimes(number, selector) {}
+function printNPrimes(number, selector) {
+    const tableBody = document.querySelector(`${selector} tbody`);
+    const primes = getNPrimes(number);
+
+    tableBody.innerHTML = ''; 
+
+    primes.forEach((prime) => {
+        const row = document.createElement('tr');
+        const cell = document.createElement('td');
+        cell.textContent = prime;
+        row.appendChild(cell);
+        tableBody.appendChild(row);
+    });
+}
 
 
 /**
@@ -55,13 +100,17 @@ function printNPrimes(number, selector) {}
  * @param {Object} urlParams URL parameters
  * @param {string} selector element to use for display
  */
-function displayWarnings(urlParams, selector) {}
+function displayWarnings(urlParams, selector) {
+    
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get('name') || 'student';
+const number = parseInt(urlParams.get('number')) || 330;
 
 window.onload = function () {
     // TODO: Initialize the following variables
-    let urlParams = "";
-    let name = "";
-    let number = "";
+
     this.displayWarnings(urlParams, "#warnings");
     greet(name, "#greeting");
     printNumberInfo(number, "#numberInfo");
