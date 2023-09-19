973e7d28c861ff9c99d2f0ca81bebb2c6e4cae98
Mughees Warraich <m.mughees@Mugheess-Air.lc.luther.edu>
2023-09-19 11:01:34 -0500

    add calculator

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -11,19 +11,45 @@ var clearOnEntry;
  * 
  * @param {number} digit digit to add or display on the `outputScreen`
  */
-function enterDigit(digit) {}
+function enterDigit(digit) {
+    if (clearOnEntry) {
+        outputScreen.textContent = '';
+        clearOnEntry = false;
+    }
+    outputScreen.textContent += digit;
+    expression += digit;
+}
 
 
 /**
  * Clear `outputScreen` and set value to 0
  */
-function clear_screen() {}
+function clear_screen() {
+    outputScreen.textContent = '';
+    expression = '';
+    clearOnEntry = true;
+}
 
 
 /**
  * Evaluate the expression and display its result or *ERROR*
  */
-function eval_expr() {}
+function eval_expr() {
+    try {
+        var result = eval(expression);
+        if (isNaN(result) || result === Infinity) {
+            outputScreen.textContent = 'ERROR';
+        } else {
+            outputScreen.textContent = result;
+        }
+        expression = result.toString();
+        clearOnEntry = true;
+    } catch (error) {
+        outputScreen.textContent = 'ERROR';
+        expression = '';
+        clearOnEntry = true;
+    }
+}
 
 
 /**
@@ -31,7 +57,14 @@ function eval_expr() {}
  * 
  * @param {string} operation to add to the expression
  */
-function enterOp(operation) {}
+function enterOp(operation) {
+    if (clearOnEntry) {
+        outputScreen.textContent = '';
+        clearOnEntry = false;
+    }
+    outputScreen.textContent += operation;
+    expression += operation;
+}
 
 
 window.onload = function () {
