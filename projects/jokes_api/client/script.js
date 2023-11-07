386cb93ccff98cff8e6fff0653e41d5876b20491
Mughees Warraich <m.mughees@Mugheess-Air.lc.luther.edu>
2023-11-07 04:52:24 -0600

    push

--- projects/jokes_api/client/script.js
+++ projects/jokes_api/client/script.js
@@ -1,2 +1,108 @@
 'use strict';
 
+var apiUrl = 'http://localhost:5000/api/v1/jokes';
+var apiUrl2 = 'http://localhost:5000/api/v1/jokes/';
+
+// Populate the category select element
+var selCat = document.getElementById('selCat');
+var categories = ['all', 'chuck', 'neutral'];
+for (var i = 0; i < categories.length; i++) {
+    var option = document.createElement('option');
+    option.text = categories[i];
+    option.value = categories[i];
+    selCat.add(option);
+}
+
+// Populate the language select element
+var selLang = document.getElementById('selLang');
+var languages = ['en', 'es', 'de'];
+for (var i = 0; i < languages.length; i++) {
+    var option = document.createElement('option');
+    option.text = languages[i];
+    option.value = languages[i];
+    selLang.add(option);
+}
+
+var selNum = document.getElementById('selNum');
+var categories = ['1', '2', '3', '5', '10'];
+for (var i = 0; i < categories.length; i++) {
+    var option = document.createElement('option');
+    option.text = categories[i];
+    option.value = categories[i];
+    selNum.add(option);
+
+}
+
+// Add event listener to the form submit button
+var btnAmuse = document.getElementById('btnAmuse');
+btnAmuse.addEventListener('click', function(event) {
+    event.preventDefault();
+    var category = selCat.value;
+    var language = selLang.value;
+    var number = selNum.value;
+    if (!number) {
+        var jokesDiv = document.getElementById('jokes');
+        jokesDiv.innerHTML = '<p>Please enter a value for number</p>';
+        return;
+    }
+    if (!Number.isInteger(Number(number))) {
+        var jokesDiv = document.getElementById('jokes');
+        jokesDiv.innerHTML = '<p>Please enter a valid integer for Number</p>';
+        return;
+    }
+
+    var url = apiUrl + '?category=' + category + '&language=' + language + '&number=' + number;
+    fetch(url)
+        .then(response => response.json())
+        .then(function(data) {
+            if (data.error) {
+                var jokesDiv = document.getElementById('jokes');
+                jokesDiv.innerHTML = '<p>' + data.error + '</p>';
+            } else {
+            var jokes = data.jokes;
+            var jokesDiv = document.getElementById('jokes');
+            jokesDiv.innerHTML = '';
+            for (var i = 0; i < jokes.length; i++) {
+                var jokeDiv = document.createElement('div');
+                jokeDiv.innerHTML = '<p>' + jokes[i].joke + '</p>';
+                jokesDiv.appendChild(jokeDiv);
+            }}
+        })
+        .catch(function(error) {
+            var jokesDiv = document.getElementById('jokes');
+            jokesDiv.innerHTML = '<p>' + error.message + '</p>';
+        });
+});
+
+
+// Add event listener to the form submit button
+var btnAmuse2 = document.getElementById('btnAmuse2');
+btnAmuse2.addEventListener('click', function(event) {
+    event.preventDefault();
+    var category = selCat.value;
+    var language = selLang.value;
+    var id = selid.value;
+    if (!id) {
+        var jokesDiv = document.getElementById('jokes');
+        jokesDiv.innerHTML = '<p>Please enter a value for id</p>';
+        return;
+    }
+    var url = apiUrl2 + language + '/' + category + '/' + id;
+    fetch(url)
+        .then(response => response.json())
+        .then(function(data) {
+            if (data.error) {
+                var jokesDiv = document.getElementById('jokes');
+                jokesDiv.innerHTML = '<p>' + data.error + '</p>';
+            } else {
+                var jokes = data.joke;
+                var jokesDiv = document.getElementById('jokes');
+                jokesDiv.innerHTML = '';
+                jokesDiv.innerHTML = jokes;
+            }
+        })
+        .catch(function(error) {
+            var jokesDiv = document.getElementById('jokes');
+            jokesDiv.innerHTML = '<p>' + error.message + '</p>';
+        });
+})
--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -2,3 +2,42 @@
 """
 jokes api
 """
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import pyjokes
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/v1/jokes', methods=['GET'])
+def get_jokes():
+    category = request.args.get('category')
+    language = request.args.get('language')
+    number = int(request.args.get('number'))
+    if category == 'chuck' and language == 'es':
+        return jsonify({'error': 'No jokes available for category "chuck" and language "es"'}), 404
+    jokes = pyjokes.get_jokes(category=category, language=language)
+    if not jokes:
+        return jsonify({'error': 'Invalid category or language'}), 404
+    if number > len(jokes):
+        return jsonify({'error': 'Number of jokes requested exceeds the total number of jokes available'}), 404
+    random_jokes = []
+    for i in range(number):
+        random_jokes.append({'joke': pyjokes.get_joke(category=category, language=language)})
+    return jsonify({'jokes': random_jokes})
+
+@app.route('/api/v1/jokes/<string:language>/<string:category>/<int:id>', methods=['GET'])
+def get_joke(language, category, id):
+    if category == 'chuck' and language == 'es':
+        return jsonify({'error': 'No jokes available for category "chuck" and language "es"'}), 404
+    jokes = pyjokes.get_jokes(category=category, language=language)
+    if not jokes:
+        return jsonify({'error': 'Invalid category or language'}), 404
+    if id >= len(jokes):
+        return jsonify({'error': 'Joke ID not found'}), 404
+    return jsonify({'id': id, 'joke': jokes[id]})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
